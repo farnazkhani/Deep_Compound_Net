051e33e6c51497d94b4454a450a6e9cb8945f01f
farnazkhani <milad.alipour.9877@gmail.com>
2023-07-24 18:40:45 +0330

    data

---
+++ Dataset/data_multi/omics_protein.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from node2vec import Node2Vec
+import networkx as nx
+import pandas as pd
+import pickle
+
+# Load dataset of omics data (protein-protein interaction data or compound-compound interaction data)
+cc = pd.read_csv('protein_protein_interaction.csv')
+
+# Build the no-directed graph
+graph_cc = nx.Graph()
+
+# -> add weighted edges
+for j in range(len(cc['protein1'])):
+    graph_cc.add_edge(cc['protein1'].iloc[j], cc['protein2'].iloc[j], weight=cc['combined_score'].iloc[j])
+    
+    
+# Precompute probabilities and generate walks
+node2vec = Node2Vec(graph_cc)
+
+# Embed
+model = node2vec.fit()  # Any keywords acceptable by gensim.Word2Vec can be passed, `diemnsions` and `workers` are automatically passed (from the Node2Vec constructor)
+
+# Save the model of Node2vec
+with open('modelpp.pickle', mode='wb') as f:
+    pickle.dump(model, f)
+
